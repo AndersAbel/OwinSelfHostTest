fd9f2008967ee2c9d580ddefadfeabc4a322d027
Anders Abel <anders@abel.nu>
2014-05-19 11:15:29 +0200

    Minimalistic working owin self host.

---
+++ OwinSelfHostTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinSelfHostTest
+{
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start("http://localhost:4242", (app) =>
+                {
+                    app.Use(new Func<AppFunc, AppFunc>(next => (async context =>
+                        {
+                            using (var writer = new StreamWriter(context["owin.ResponseBody"] as Stream))
+                            {
+                                await writer.WriteAsync("Hello World!");
+                            }
+                        })));
+                }))
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
